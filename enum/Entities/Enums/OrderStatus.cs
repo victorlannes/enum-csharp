58fd49d9cd137dd3d178651cf7fe62dd755194f6
Victor Lannes <victor.ferreiralannes@hotmail.com>
2023-01-17 14:19:46 -0300

    Adicionar arquivos de projeto.

---
+++ enum/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace  Exercicio.Entities.Enums
+{
+     enum OrderStatus : int
+        {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Deçivered = 3,
+}
+}
---
+++ enum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Exercicio.Entities;
+using Exercicio.Entities.Enums;
+
+namespace Exercicio{
+    class Program {
+        static void Main(string[] args) {
+
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+        }
+    }
+}
